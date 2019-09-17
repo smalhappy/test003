e27ecd112ff484099a8cdb8b9e5d1f072e13ca9b
wxl <wangxiaolecc@aliyun.com>
2019-09-17 17:07:13 +0800

    test

---
+++ src/main/java/com/test/demo/test003/Test003Application.java
@@ -0,0 +1,22 @@
+package com.test.demo.test003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Test003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test003Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(){
+
+        return "sss";
+    }
+
+}
